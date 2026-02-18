2685167c835dce4f66054e06b3b3278702f13bab
Lucas Smelley <lskdw07@gmail.com>
2026-02-18 13:19:55 -0600

    creanup

--- scripts/door.gd
+++ scripts/door.gd
@@ -8,8 +8,6 @@ func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	
 func _on_body_entered(body: Node) -> void:
-	print("Door triggered: ", exit_side)
-
 	if _used: 
 		return
 	if not body.is_in_group("player"):
