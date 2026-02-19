ba794a48b602fe2b8805cb070fd7bb206ee171fa
Buzzcut490 <bradensteinerstl@gmail.com>
2026-02-18 22:10:42 -0600

    Spike changes

--- scripts/spike.gd
+++ scripts/spike.gd
@@ -1,14 +1,13 @@
 extends Area2D
 
-@export var player_node: NodePath
-var player: Node = null 
+
 
 func _ready():
 	body_entered.connect(_on_body_entered)
 	
-	if player != null:
-		player = get_node(player_node)
+	
 	
 func _on_body_entered(body):
-	if body == player:
-		player.respawn()
+	if body.is_in_group("player"):
+		body.respawn()
+		print("hit")
